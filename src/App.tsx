2c2d96fcfc551f1c9cd821c42962e35ee5055bc1
Gabriel Loye <gabrielloye@hotmail.com>
2020-05-17 02:32:20 +0800

    shift aws exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
 /** Components */
 import AppRouter from './navigation/routes';
 
 import './App.css';
-
-Amplify.configure(aws_exports);
 
 const App: React.FC = () => {
   return (
